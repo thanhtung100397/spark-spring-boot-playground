f75428cb91a5822438f539c6e5f3fc435adecda2
Tran Thanh Tung <thanhtung100397@gmail.com>
2021-12-24 15:07:02 +0700

    Complete ReportTask1

--- src/main/scala/com/tungtt/bigdata/ReportTask1.scala
+++ src/main/scala/com/tungtt/bigdata/ReportTask1.scala
@@ -1,40 +1,50 @@
 package com.tungtt.bigdata
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 
 object ReportTask1 {
 
-  def parseDataRawRow(rawDataRow: String) = {
+  def parseDataRawRow(rawDataRow: String): String = {
     val rawDataParts = rawDataRow.split("\"")
     if (rawDataParts.length > 1) {
       rawDataParts(1)
     } else {
-      None
+      ""
     }
   }
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder()
-      .master("local[1]")
-      .appName("Report Task 1")
-      .getOrCreate()
+                .master("local[1]")
+                .appName("Report Task 1")
+                .getOrCreate()
 
-    val rawDataRdd = spark.sparkContext.textFile("data/Marvel-names.txt")
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
 
-    val dataRdd = rawDataRdd.map(rawDataRow => parseDataRawRow(rawDataRow))
-      .filter(data => data != None)
+    val rawDataRdd: RDD[String] = spark.sparkContext.textFile("data/Marvel-names.txt")
 
-    println(dataRdd.count())
+    val dataRdd: RDD[String] = rawDataRdd.map(rawDataRow => parseDataRawRow(rawDataRow))
+                               .filter(data => !data.isEmpty)
 
-//    val db = spark.read
-//      .format("jdbc")
-//      .option("url", "jdbc:postgresql://:/")
-//      .option("driver", "org.postgresql.Driver")
-//      .option("dbtable", "")
-//      .option("user", "")
-//      .option("password", "")
-//      .load()
-//
-//    println(db)
+    val heroDF: DataFrame = dataRdd.toDF("hero_name")
+    val heroTotalDisplayDS: Dataset[Row] = heroDF.groupBy("hero_name")
+                                           .agg(
+                                                 count("hero_name").as("total_number")
+                                               )
+                                           .sort(desc("total_number"))
+    heroTotalDisplayDS.show()
+    println(heroTotalDisplayDS.count())
+
+    heroTotalDisplayDS.write
+      .format("jdbc")
+      .mode("overwrite")
+      .option("url", "jdbc:postgresql:///test")
+      .option("driver", "org.postgresql.Driver")
+      .option("dbtable", "test_report")
+      .option("user", "")
+      .option("password", "")
+      .save()
   }
 }
