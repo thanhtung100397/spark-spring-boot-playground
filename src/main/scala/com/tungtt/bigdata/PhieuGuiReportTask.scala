22f536b8194edb25277407071f14c18ac2afc328
Tran Thanh Tung <thanhtung100397@gmail.com>
2022-01-14 17:51:22 +0700

    Update SyncDataStruct

--- src/main/scala/com/tungtt/bigdata/PhieuGuiReportTask.scala
+++ src/main/scala/com/tungtt/bigdata/PhieuGuiReportTask.scala
@@ -1,23 +1,42 @@
 package com.tungtt.bigdata
 
 import com.tungtt.bigdata.models.{PhieuGui, SyncData}
-import org.apache.spark.sql.{Dataset, SaveMode, SparkSession}
-import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+import org.apache.spark.sql.{Dataset, Encoders, SaveMode, SparkSession}
+import org.apache.spark.sql.types.{DecimalType, IntegerType, LongType, StringType, StructType}
+
+import java.math.BigInteger
 
 object PhieuGuiReportTask {
 
   val phieuGuiStruct: StructType = new StructType()
     .add("payload", new StructType()
       .add("after", new StructType()
-        .add("id_phieugui", IntegerType, nullable = true)
         .add("ma_phieugui", StringType, nullable = true)
-        .add("gui_trongnuoc", IntegerType, nullable = true)
         .add("ma_khgui", StringType, nullable = true)
-        .add("ten_khgui", StringType, nullable = true)
-        .add("diachi_khgui", StringType, nullable = true)
-        .add("tel_khgui", StringType, nullable = true),
-        nullable = true),
-      nullable = true)
+        .add("ngay_nhap_may", StringType, nullable = true)
+        .add("tong_tien", DecimalType(0,9), nullable = true)
+        .add("tong_vat", new StructType()
+          .add("scale", IntegerType, nullable = true)
+          .add("value", StringType, nullable = true)
+          ,
+        nullable = true)
+        ,
+      nullable = false)
+      .add("source", new StructType()
+        .add("ts_ms", LongType, nullable = true)
+        ,
+        nullable = false)
+      ,
+    nullable = false)
+
+  case class PhieuGui(var ma_phieugui: String,
+                      var ma_khgui: String,
+                      var ngay_nhap_may: String,
+                      var tong_tien: BigDecimal,
+                      var tong_vat: BigDecimal,
+                      var timestamp: Long) {
+
+  }
 
   val postgresqlSinkOptions: Map[String, String] = Map(
     "dbtable" -> "phieu_gui_kafka",
@@ -44,8 +63,18 @@ object PhieuGuiReportTask {
              .load()
 
     val ds = df.selectExpr("CAST(value AS STRING)").as[String]
-               .select(from_json($"value", phieuGuiStruct).as[SyncData[PhieuGui]])
-               .map(syncData => syncData.payload.after)
+               .select(from_json($"value", phieuGuiStruct).alias("value"))
+               .map(syncData => {
+                 println(syncData.get(0).getClass);
+                 PhieuGui("1", "a", "a", BigDecimal(1), BigDecimal(2), 1234567)
+               })
+//               .groupBy(
+//                 window($"timestamp", "1 minutes", "5 minutes"),
+//                 col("ma_phieugui")
+//               )
+//               .agg(Map(
+//                 "ma_phieugui" -> "count",
+//               ))
 
 //    ds.writeStream
 //      .foreachBatch((dataSet: Dataset[PhieuGui], batchId: Long) => {
